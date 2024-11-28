3e4d3361cc1d4d0bb91659253a12b68bfc7eff9b
Samkwizera <s.ihimbazwe@alustudent.com>
2024-11-28 14:33:59 -0800

    13-model_state_delete_a.py

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
-""" module delete states """
-
+"""
+Deletes all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+    # Retrieve arguments: username, password, and database name
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # Connect to the MySQL database
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}', pool_pre_ping=True)
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
-    st = session.query(State).filter(State.name.like('%a%')).all()
-    if st:
-        for stat in st:
-            session.delete(stat)
-        session.commit()
+
+    # Delete all State objects with a name containing the letter 'a'
+    session.query(State).filter(State.name.like('%a%')).delete(synchronize_session=False)
+
+    # Commit the changes to the database
+    session.commit()
+
+    # Close the session
     session.close()
+
