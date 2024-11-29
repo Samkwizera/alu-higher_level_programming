278226976b269c455b4071d9e077b427566b475e
Skwizera <s.ihimbazwe@alustudent.com>
2024-11-29 22:55:16 +0200

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ Base = declarative_base().
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
+ 
 Base = declarative_base()
 
 
