7234ac3c14062dca5ef8c8e734ef0ba942802bf1
Samkwizera <s.ihimbazwe@alustudent.com>
2025-01-23 10:54:25 -0800

    10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0], 10);
+
+const factorial = (n) => {
+    if (n <= 1 || isNaN(n)) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+};
+
+console.log(factorial(num));
+
