eb8764a7c5813ab36c53af8ffd6320352338757f
Samkwizera <s.ihimbazwe@alustudent.com>
2025-01-23 10:39:05 -0800

    4-concat.js

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const firstArg = args[0] || 'undefined';
+const secondArg = args[1] || 'undefined';
+
+console.log(`${firstArg} is ${secondArg}`);
+
