27caf0cfca2e9937609fd33a323a57ea01b3a5a5
Samkwizera <s.ihimbazwe@alustudent.com>
2025-01-23 12:07:58 -0800

    2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,20 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (w, h) {
+
+class Rectangle {
+  constructor(w, h) {
+    if (
+      typeof w !== 'number' ||
+      typeof h !== 'number' ||
+      w <= 0 ||
+      h <= 0 ||
+      !Number.isInteger(w) ||
+      !Number.isInteger(h)
+    ) {
+      return {};
+    }
     this.width = w;
     this.height = h;
   }
-};
+}
+
+module.exports = Rectangle;
