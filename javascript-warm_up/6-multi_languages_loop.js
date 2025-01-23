f1a9dbd9839cea73dd0b7ece07a5f36bdd968cab
Samkwizera <s.ihimbazwe@alustudent.com>
2025-01-23 11:34:34 -0800

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let output = '';
+const strs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < languages.length; i++) {
-  output += `${languages[i]}\n`;
+for (const str of strs) {
+  console.log(str);
 }
-
-console.log(output.trim());
